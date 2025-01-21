31e58cd2028ff53e25502b1014120493c3b36fcd
baxter <1772587081@qq.com>
2025-01-21 21:01:08 +0800

    通讯录

--- learn/tongxunlu/tongxun.cpp
+++ learn/tongxunlu/tongxun.cpp
@@ -84,6 +84,192 @@ void showPerson(struct tongxunlu *txl)
     }
 }
 
+// 检测联系人是否存在，存在的话则显示序号，不存在则显示-1
+int isExist(struct tongxunlu *txl, string name)
+{
+    for (int i = 0; i < txl->size; i++)
+    {
+        if (txl->personArray[i].name == name)
+            return i;
+    }
+    return -1;
+}
+// 删除联系人
+void deletePerson(struct tongxunlu *txl)
+{
+    if (txl->size == 0)
+    {
+        cout << "通讯录里没有人员信息" << endl;
+        system("pause");
+        system("cls");
+        return;
+    }
+    cout << "请输入要删除的联系人的姓名" << endl;
+    string name;
+    cin >> name;
+    int ret = isExist(txl, name);
+    if (ret != -1)
+    {
+        // 进行删除操作
+        // 循环将后面一位的内容覆盖前一位，并进行总数减1
+        for (int i = ret; i < txl->size; i++)
+        {
+            txl->personArray[i] = txl->personArray[i + 1];
+        }
+        txl->size--;
+        cout << "删除成功" << endl;
+    }
+    else
+    {
+        cout << "通讯录里没有此人" << endl;
+    }
+    system("pause");
+    system("cls");
+}
+
+// 查找
+void findPerson(struct tongxunlu *txl)
+{
+    cout << "请输入要查找的联系人" << endl;
+    string name;
+    cin >> name;
+    int ret = isExist(txl, name);
+    if (ret != -1)
+    {
+        cout << "姓名 " << txl->personArray[ret].name << "\t";
+        cout << "性别 " << (txl->personArray[ret].sex == 1 ? "男" : "女") << "\t";
+        cout << "年龄 " << txl->personArray[ret].age << "\t";
+        cout << "电话 " << txl->personArray[ret].phone << "\t";
+        cout << "地址 " << txl->personArray[ret].address << endl;
+    }
+    else
+        cout << "查无此人" << endl;
+    system("pause");
+    system("cls");
+}
+
+// 修改
+void changePerson(struct tongxunlu *txl)
+{
+    cout << "请输入要修改的联系人" << endl;
+    string name;
+    cin >> name;
+    int ret = isExist(txl, name);
+    if (ret == -1)
+    {
+        cout << "查无此人" << endl;
+    }
+    else
+    {
+        cout << "请输入想要修改的选项" << endl;
+        cout << "1.修改姓名" << endl;
+        cout << "2.修改性别" << endl;
+        cout << "3.修改年龄" << endl;
+        cout << "4.修改电话" << endl;
+        cout << "5.修改地址" << endl;
+        cout << "6.修改多项或全部" << endl;
+        int swi = 0;
+        cin >> swi;
+        switch (swi)
+        {
+        case 1:
+        {
+            cout << "请输入改后的姓名" << endl;
+            string name;
+            cin >> name;
+            txl->personArray[ret].name = name;
+            break;
+        }
+        case 2:
+        {
+            cout << "请输入改后的性别" << endl;
+            cout << "1-----男" << endl;
+            cout << "2-----女" << endl;
+            int sex;
+            cin >> sex;
+            txl->personArray[ret].sex = sex;
+            break;
+        }
+        case 3:
+        {
+            cout << "请输入改后的年龄" << endl;
+            int age;
+            cin >> age;
+            txl->personArray[ret].age = age;
+            break;
+        }
+        case 4:
+        {
+            cout << "请输入改后的电话" << endl;
+            string phone;
+            cin >> phone;
+            txl->personArray[ret].phone = phone;
+            break;
+        }
+        case 5:
+        {
+            cout << "请输入改后的地址" << endl;
+            string addres;
+            cin >> addres;
+            txl->personArray[ret].address = addres;
+            break;
+        }
+        case 6:
+        {
+            cout << "请输入改后的姓名" << endl;
+            string name;
+            cin >> name;
+            txl->personArray[ret].name = name;
+
+            cout << "请输入改后的性别" << endl;
+            cout << "1-----男" << endl;
+            cout << "2-----女" << endl;
+            int sex;
+            cin >> sex;
+            txl->personArray[ret].sex = sex;
+
+            cout << "请输入改后的年龄" << endl;
+            int age;
+            cin >> age;
+            txl->personArray[ret].age = age;
+
+            cout << "请输入改后的电话" << endl;
+            string phone;
+            cin >> phone;
+            txl->personArray[ret].phone = phone;
+
+            cout << "请输入改后的地址" << endl;
+            string addres;
+            cin >> addres;
+            txl->personArray[ret].address = addres;
+            break;
+        }
+        }
+        system("pause");
+        system("cls");
+    }
+}
+
+// 清空,直接令数组长度为0
+void cleanPerson(struct tongxunlu *txl)
+{
+    cout << "你确定要清空通讯录吗" << endl;
+    string pan;
+    cout << "输入“Yes”来确认清空" << endl;
+    cin >> pan;
+    if (pan == "Yes")
+    {
+        txl->size = 0;
+        cout << "通讯录已清空" << endl;
+    }
+    else
+    {
+        cout << "取消清空" << endl;
+    }
+    system("pause");
+    system("cls");
+}
+
 void showmenu()
 {
     cout << "*************" << endl;
@@ -116,16 +302,16 @@ int main()
             showPerson(&txl);
             break;
         case 3:
-            /* code */
+            deletePerson(&txl);
             break;
         case 4:
-            /* code */
+            findPerson(&txl);
             break;
         case 5:
-            /* code */
+            changePerson(&txl);
             break;
         case 6:
-            /* code */
+            cleanPerson(&txl);
             break;
         case 0:
             cout << "欢迎下次使用" << endl;
