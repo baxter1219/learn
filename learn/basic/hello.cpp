6cb880aed379cb2425888bbed6ddef1b06671a8c
baxter <1772587081@qq.com>
2025-01-21 21:41:33 +0800

    面向对象的编程

---
+++ learn/basic/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>  //引入头文件
+using namespace std; // 工作空间
+int main()           // 主函数
+{
+    cout << "hello world i am 18 Today!" << endl; // 显示
+    /*多行注释
+    cout==
+    endl==endline
+    */
+    // 单行注释
+    system("pause"); // 不直接退出，需要额外按键退出程序
+    return 0;
+}
